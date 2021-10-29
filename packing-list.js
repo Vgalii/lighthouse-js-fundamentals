1cb0881d700c5a0d21adf63176237bdc80f5bc20
Vgalii <veronicaggalii@gmail.com>
2021-10-29 16:41:52 -0400

    Using for loop

--- packing-list.js
+++ packing-list.js
@@ -5,11 +5,5 @@ console.log("Kitchen stuff to pack:");
 for (let i = 0; i < packingList.length; i++) {
   console.log(packingList[i]);
 }
-// while loop
-console.log("Kitchen stuff to pack:");
-let i = 0;
-while (i < packingList.length) {
-  console.log(packingList[i]);
-  i++;
-}
+
 
