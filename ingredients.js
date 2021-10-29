bfbf31f922c255b27c74da10fc7f6ea19b68f6ca
Vgalii <veronicaggalii@gmail.com>
2021-10-29 17:36:58 -0400

    Iterating over arrays

---
+++ ingredients.js
@@ -0,0 +1,17 @@
+const ingredients = ["eggs", "milk", "flour", "sugar", "baking soda", "baking powder", "chocolate chips", "bananas"];
+for (let i = 0; i < ingredients.length; i++){
+  console.log(ingredients[i])
+}
+//while loop
+let i = 0;
+while (i < ingredients.length){
+  console.log(ingredients[i]);
+  i++;
+}
+
+//for loop 
+
+const ingredientsBackwords = ["eggs", "milk", "flour", "sugar", "baking soda", "baking powder", "chocolate chips", "bananas"].reverse();
+for (let i = 0; i < ingredientsBackwords.length; i++){
+  console.log(ingredientsBackwords[i]);
+}
